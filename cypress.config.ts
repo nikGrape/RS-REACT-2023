4c809205117ab08a92aab4acd2a60000e0ab8de1
Nikolai Vinogradov <nik@Nikolais-MacBook-Pro.local>
2023-04-29 21:53:45 -0700

    rafactor: cleanup

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable-file @typescript-eslint/no-unused-vars */
 import { defineConfig } from 'cypress';
 import coverage from '@cypress/code-coverage/task';
 
